307e5d8eef32d9c27704cbc3368dd9d5ed698b14
lcq <lcqtdwj@gmail.com>
2012-04-05 21:16:34 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BBBlog::Application.config.secret_token = 'a270d6650ef9c7c9e0e686eff774164bd171090fd84490d99f53a8ee7ad2a5657ea8e66b9bca45b8106f77a5ff321968a028def4fff66a05735462ff932a45a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BBBlog::Application.config.session_store :cookie_store, key: '_BBBlog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BBBlog::Application.config.session_store :active_record_store
